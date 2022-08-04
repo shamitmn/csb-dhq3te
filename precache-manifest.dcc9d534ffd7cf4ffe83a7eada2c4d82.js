6b217f15b779a2cd7378bafa612b064dcb3e4279
Code Sandbox <hello@codesandbox.io>
2022-08-04 18:24:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dcc9d534ffd7cf4ffe83a7eada2c4d82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627de6b1d0eb748a1bba18d643f54483",
+    "url": "https://shamitmn.github.io/csb-dhq3te/index.html"
+  },
+  {
+    "revision": "eca6148e5a98c034090a",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/css/main.b38bc5d1.chunk.css"
+  },
+  {
+    "revision": "80704e6a77ffa341e4c2",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/js/2.f16717de.chunk.js"
+  },
+  {
+    "revision": "0b285f52204d775f07fee2d388be9b90",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/js/2.f16717de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca6148e5a98c034090a",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/js/main.3d47c74f.chunk.js"
+  },
+  {
+    "revision": "99dc5945a82fba554b23",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/js/runtime-main.0f169644.js"
+  },
+  {
+    "revision": "0773f883daed1ce3dc0251c191188512",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/media/addres.0773f883.png"
+  },
+  {
+    "revision": "c337150d8120562fbe05b57b35de7ce5",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/media/email.c337150d.png"
+  },
+  {
+    "revision": "ff553063559acee9f725c24d7f377b1f",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/media/me.ff553063.jpg"
+  },
+  {
+    "revision": "540fcabe0138e7cf0dff340501cdede1",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/media/moon.540fcabe.png"
+  },
+  {
+    "revision": "3eb2e4d220f7a16ec40f03aa49f0cd80",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/media/phone.3eb2e4d2.png"
+  },
+  {
+    "revision": "6f05073614e5532d535459592e423479",
+    "url": "https://shamitmn.github.io/csb-dhq3te/static/media/sun.6f050736.png"
+  }
+]);
